c8d26a9dc41ad22899ddbf8428af4d74f3dffd6d
Donald Carr <d@chaos-reins.com>
2018-04-10 20:21:03 -0700

    Experiment with surface management

--- main.cpp
+++ main.cpp
@@ -6,18 +6,51 @@
 #include <QThread>
 #include <QMetaObject>
 
+#include <QSettings>
+
 int main ( int argc, char*argv[] )
 {
-    //Raspberry Pi defaults to 16bpp
+    QGuiApplication app(argc, argv);
+    app.setOrganizationName("Chaos Reins");
+    app.setApplicationName("projectmofo");
 
-    QSurfaceFormat format = QSurfaceFormat::defaultFormat();
-    format.setAlphaBufferSize(0);
-    format.setRedBufferSize(8);
-    format.setGreenBufferSize(8);
-    format.setBlueBufferSize(8);
-    QSurfaceFormat::setDefaultFormat(format);
+    QSettings settings;
 
-	QGuiApplication app ( argc, argv );
+    if (settings.value("force32bpp", true).toBool()) {
+        qDebug() << "force 32bpp";
+        // Raspberry Pi defaults to 16bpp
+        QSurfaceFormat format = QSurfaceFormat::defaultFormat();
+        format.setAlphaBufferSize(0);
+        format.setRedBufferSize(8);
+        format.setGreenBufferSize(8);
+        format.setBlueBufferSize(8);
+        QSurfaceFormat::setDefaultFormat(format);
+    } else if (settings.value("force16bpp", true).toBool()) {
+        qDebug() << "force 16bpp";
+        QSurfaceFormat format = QSurfaceFormat::defaultFormat();
+        format.setAlphaBufferSize(0);
+        format.setRedBufferSize(5);
+        format.setGreenBufferSize(6);
+        format.setBlueBufferSize(5);
+        QSurfaceFormat::setDefaultFormat(format);
+    }
+
+    if (settings.value("forceSingleBuffer", false).toBool()) {
+        qDebug() << "force single buffer";
+        QSurfaceFormat format = QSurfaceFormat::defaultFormat();
+        format.setSwapBehavior(QSurfaceFormat::SingleBuffer);
+        QSurfaceFormat::setDefaultFormat(format);
+    } else if (settings.value("forceDoubleBuffer", true).toBool()) {
+        qDebug() << "force double buffer";
+        QSurfaceFormat format = QSurfaceFormat::defaultFormat();
+        format.setSwapBehavior(QSurfaceFormat::DoubleBuffer);
+        QSurfaceFormat::setDefaultFormat(format);
+    }else if (settings.value("forceTripleBuffer", true).toBool()) {
+        qDebug() << "force triple buffer";
+        QSurfaceFormat format = QSurfaceFormat::defaultFormat();
+        format.setSwapBehavior(QSurfaceFormat::TripleBuffer);
+        QSurfaceFormat::setDefaultFormat(format);
+    }
 
     ProjectMWindow window;
     window.resize(640, 480);
