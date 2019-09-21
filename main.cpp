0cfae2bc47bfebd8dc0055c1d25c0bece46920d4
Donald Carr <d@chaos-reins.com>
2019-09-21 02:25:52 -0700

    Hide the cursor

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@
 int main ( int argc, char*argv[] )
 {
     QGuiApplication app(argc, argv);
+    QGuiApplication::setOverrideCursor(Qt::BlankCursor);
     app.setOrganizationName("Chaos Reins");
     app.setApplicationName("projectmofo");
 
